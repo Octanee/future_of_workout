c8bf453bfecdc593673456377ae9ebabc05d1e68
Kamil Boryczka <leoctaneee@gmail.com>
2022-08-31 12:45:34 +0200

    exercise_provider

--- lib/models/enum/fatigue.dart
+++ lib/models/enum/fatigue.dart
@@ -7,8 +7,8 @@ enum Fatigue {
   fatigue40(color: Color(0xFFf5d084)),
   fatigue55(color: Color(0xFFf6c45d)),
   fatigue70(color: Color(0xFFf7bf4d)),
-  fatigue85(color: Color(0xFFfaa805));
-  //fatigue100(color: Color(0xFFee2222));
+  fatigue85(color: Color(0xFFfaa805)),
+  fatigue100(color: Color(0xFFee2222));
 
   const Fatigue({required this.color});
   final Color color;
