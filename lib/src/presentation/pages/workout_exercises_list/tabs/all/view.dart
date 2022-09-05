07108387d43052dfe00aea23d8e08975e0927d99
Kamil Boryczka <leoctaneee@gmail.com>
2022-09-05 23:49:34 +0200

    todo

--- lib/src/presentation/pages/workout_exercises_list/tabs/all/view.dart
+++ lib/src/presentation/pages/workout_exercises_list/tabs/all/view.dart
@@ -14,6 +14,7 @@ class WorkoutExercisesListAllView extends StatelessWidget {
       child: Column(
         children: [
           _searchBar(),
+          // TODO Exercise GridView 
         ],
       ),
     );
