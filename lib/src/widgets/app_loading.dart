817f523fae863dfa7282c1a1c6957d0056758701
Kamil Boryczka <leoctaneee@gmail.com>
2022-09-10 23:25:28 +0200

    Removal AppScaffold form AppLoading widget

--- lib/src/widgets/app_loading.dart
+++ lib/src/widgets/app_loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:future_of_workout/src/styles/styles.dart';
-import 'package:future_of_workout/src/widgets/widgets.dart';
 
 class AppLoading extends StatelessWidget {
   const AppLoading({this.text, super.key});
@@ -9,23 +8,21 @@ class AppLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppScaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CircularProgressIndicator(
-              color: AppColors.yellow,
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(
+            color: AppColors.yellow,
+          ),
+          const SizedBox(height: 16),
+          if (text != null)
+            Text(
+              text!,
+              style: AppTextStyle.semiBold20,
+              textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 16),
-            if (text != null)
-              Text(
-                text!,
-                style: AppTextStyle.semiBold20,
-                textAlign: TextAlign.center,
-              ),
-          ],
-        ),
+        ],
       ),
     );
   }
