7f9675c6f1e20eb6e27bf02885ba1d4a130a7577
Kamil Boryczka <leoctaneee@gmail.com>
2022-09-11 11:32:05 +0200

    update workout details bloc

--- lib/src/workout_details/bloc/workout_details_bloc.dart
+++ lib/src/workout_details/bloc/workout_details_bloc.dart
@@ -1,105 +1,104 @@
-// import 'dart:async';
+import 'dart:async';
 
-// import 'package:bloc/bloc.dart';
-// import 'package:equatable/equatable.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:workout_repository/workout_repository.dart';
 
-// // import 'package:future_of_workout/src/data/models/models.dart';
-// // import 'package:future_of_workout/src/data/repositories/repositories.dart';
+part 'workout_details_event.dart';
+part 'workout_details_state.dart';
 
-// part 'workout_details_event.dart';
-// part 'workout_details_state.dart';
+class WorkoutDetailsBloc
+    extends Bloc<WorkoutDetailsEvent, WorkoutDetailsState> {
+  WorkoutDetailsBloc({
+    required WorkoutRepository workoutRepository,
+    required this.workoutId,
+  })  : _workoutRepository = workoutRepository,
+        super(const WorkoutDetailsState()) {
+    on<WorkoutDetailsLoadWorkout>(_onLoadWorkout);
+    on<WorkoutDetailsNameChanged>(_onNameChanged);
+    on<WorkoutDetailsFavoritToggled>(_onFavoritToggled);
+    on<WorkoutDetailsDelete>(_onDelete);
+  }
 
-// class WorkoutDetailsBloc
-//     extends Bloc<WorkoutDetailsEvent, WorkoutDetailsState> {
-//   WorkoutDetailsBloc({
-//     required this.repository,
-//     required this.workoutId,
-//   }) : super(const WorkoutDetailsState()) {
-//     on<WorkoutDetailsLoadWorkout>(_onLoadWorkout);
-//     on<WorkoutDetailsNameChanged>(_onNameChanged);
-//     on<WorkoutDetailsIsFavoritToggled>(_onIsFavoritToggled);
-//     on<WorkoutDetailsDelete>(_onDelete);
-//   }
+  final String workoutId;
+  final WorkoutRepository _workoutRepository;
 
-//   final String workoutId;
-//   final BaseWorkoutRepository repository;
+  Future<void> _onLoadWorkout(
+    WorkoutDetailsLoadWorkout event,
+    Emitter<WorkoutDetailsState> emit,
+  ) async {
+    emit(state.copyWith(status: WorkoutDetailsStatus.loading));
+    final workout = _workoutRepository.get(id: event.id);
 
-//   Future<void> _onLoadWorkout(
-//     WorkoutDetailsLoadWorkout event,
-//     Emitter<WorkoutDetailsState> emit,
-//   ) async {
-//     emit(state.copyWith(status: WorkoutDetailsStatus.loading));
-//     final workout = await repository.getOne(id: event.id);
+    emit(
+      state.copyWith(
+        status: WorkoutDetailsStatus.loaded,
+        workout: workout,
+      ),
+    );
+  }
 
-//     emit(
-//       state.copyWith(
-//         status: WorkoutDetailsStatus.success,
-//         workout: workout,
-//       ),
-//     );
-//   }
+  Future<void> _onNameChanged(
+    WorkoutDetailsNameChanged event,
+    Emitter<WorkoutDetailsState> emit,
+  ) async {
+    if (state.workout == null) {
+      add(WorkoutDetailsLoadWorkout(id: workoutId));
+      return;
+    }
 
-//   Future<void> _onNameChanged(
-//     WorkoutDetailsNameChanged event,
-//     Emitter<WorkoutDetailsState> emit,
-//   ) async {
-//     if (state.workout == null) {
-//       add(WorkoutDetailsLoadWorkout(id: workoutId));
-//       return;
-//     }
+    await _updateWorkout(
+      workout: state.workout!.copyWith(name: event.name),
+      emit: emit,
+    );
+  }
 
-//     await _updateWorkout(
-//       workout: state.workout!.copyWith(name: event.name),
-//       emit: emit,
-//     );
-//   }
+  Future<void> _onFavoritToggled(
+    WorkoutDetailsFavoritToggled event,
+    Emitter<WorkoutDetailsState> emit,
+  ) async {
+    if (state.workout == null) {
+      add(WorkoutDetailsLoadWorkout(id: workoutId));
+      return;
+    }
+    await _updateWorkout(
+      workout: state.workout!.copyWith(isFavorite: !state.workout!.isFavorite),
+      emit: emit,
+    );
+  }
 
-//   Future<void> _onIsFavoritToggled(
-//     WorkoutDetailsIsFavoritToggled event,
-//     Emitter<WorkoutDetailsState> emit,
-//   ) async {
-//     if (state.workout == null) {
-//       add(WorkoutDetailsLoadWorkout(id: workoutId));
-//       return;
-//     }
-//     await _updateWorkout(
-//       workout: state.workout!.copyWith(isFavorite: !state.workout!.isFavorite),
-//       emit: emit,
-//     );
-//   }
+  Future<void> _onDelete(
+    WorkoutDetailsDelete event,
+    Emitter<WorkoutDetailsState> emit,
+  ) async {
+    if (state.workout == null) {
+      add(WorkoutDetailsLoadWorkout(id: workoutId));
+      return;
+    }
 
-//   Future<void> _onDelete(
-//     WorkoutDetailsDelete event,
-//     Emitter<WorkoutDetailsState> emit,
-//   ) async {
-//     if (state.workout == null) {
-//       add(WorkoutDetailsLoadWorkout(id: workoutId));
-//       return;
-//     }
+    try {
+      await _workoutRepository.deleteWorkout(state.workout!.id);
+      emit(state.copyWith(status: WorkoutDetailsStatus.delete));
+    } catch (e) {
+      emit(state.copyWith(status: WorkoutDetailsStatus.failure));
+    }
+  }
 
-//     try {
-//       await repository.removeWorkout(id: state.workout!.id);
-//       emit(state.copyWith(status: WorkoutDetailsStatus.delete));
-//     } catch (e) {
-//       emit(state.copyWith(status: WorkoutDetailsStatus.failure));
-//     }
-//   }
-
-//   Future<void> _updateWorkout({
-//     required Workout workout,
-//     required Emitter<WorkoutDetailsState> emit,
-//   }) async {
-//     try {
-//       emit(state.copyWith(status: WorkoutDetailsStatus.updating));
-//       await repository.updateWorkout(workout: workout);
-//       emit(
-//         state.copyWith(
-//           status: WorkoutDetailsStatus.success,
-//           workout: workout,
-//         ),
-//       );
-//     } catch (e) {
-//       emit(state.copyWith(status: WorkoutDetailsStatus.failure));
-//     }
-//   }
-// }
+  Future<void> _updateWorkout({
+    required Workout workout,
+    required Emitter<WorkoutDetailsState> emit,
+  }) async {
+    try {
+      emit(state.copyWith(status: WorkoutDetailsStatus.updating));
+      await _workoutRepository.saveWorkout(workout);
+      emit(
+        state.copyWith(
+          status: WorkoutDetailsStatus.updated,
+          workout: workout,
+        ),
+      );
+    } catch (e) {
+      emit(state.copyWith(status: WorkoutDetailsStatus.failure));
+    }
+  }
+}
--- lib/src/workout_details/bloc/workout_details_state.dart
+++ lib/src/workout_details/bloc/workout_details_state.dart
@@ -1,36 +1,39 @@
-// part of 'workout_details_bloc.dart';
+part of 'workout_details_bloc.dart';
 
-// enum WorkoutDetailsStatus {
-//   initial,
-//   loading,
-//   updating,
-//   delete,
-//   success,
-//   failure,
-// }
+enum WorkoutDetailsStatus {
+  initial,
+  loading,
+  loaded,
+  updating,
+  updated,
+  delete,
+  failure,
+}
 
-// class WorkoutDetailsState extends Equatable {
-//   const WorkoutDetailsState({
-//     this.workout,
-//     this.status = WorkoutDetailsStatus.loading,
-//   });
+class WorkoutDetailsState extends Equatable {
+  const WorkoutDetailsState({
+    this.workout,
+    this.status = WorkoutDetailsStatus.initial,
+    this.workoutName = '',
+  });
 
-//   final WorkoutDetailsStatus status;
-//   final Workout? workout;
+  final WorkoutDetailsStatus status;
+  final Workout? workout;
+  final String workoutName;
 
-//   WorkoutDetailsState copyWith({
-//     WorkoutDetailsStatus? status,
-//     Workout? workout,
-//   }) {
-//     return WorkoutDetailsState(
-//       status: status ?? this.status,
-//       workout: workout ?? this.workout,
-//     );
-//   }
+  WorkoutDetailsState copyWith({
+    WorkoutDetailsStatus? status,
+    Workout? workout,
+  }) {
+    return WorkoutDetailsState(
+      status: status ?? this.status,
+      workout: workout ?? this.workout,
+    );
+  }
 
-//   @override
-//   List<Object?> get props => [
-//         status,
-//         workout,
-//       ];
-// }
+  @override
+  List<Object?> get props => [
+        status,
+        workout,
+      ];
+}
