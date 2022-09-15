8218b10961c6774c164e5ea157a1966f4adde598
Kamil Boryczka <leoctaneee@gmail.com>
2022-09-15 12:26:24 +0200

    Create color extension

---
+++ lib/src/extensions.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+extension DarkenAndLighten on Color {
+  Color darken({double amount = 0.1}) {
+    assert(amount >= 0 && amount <= 1, 'Amount should be between 0 and 1');
+
+    final hsl = HSLColor.fromColor(this);
+    final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+
+    return hslDark.toColor();
+  }
+
+  Color lighten({double amount = 0.1}) {
+    assert(amount >= 0 && amount <= 1, 'Amount should be between 0 and 1');
+
+    final hsl = HSLColor.fromColor(this);
+    final hslLight =
+        hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+
+    return hslLight.toColor();
+  }
+}
