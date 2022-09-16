f704c27f81932aba71af65206adc14e21e630fc0
Kamil Boryczka <leoctaneee@gmail.com>
2022-09-16 22:04:02 +0200

    basic scafold for workout_log_api

---
+++ packages/workout_log_api/lib/workout_log_api.dart
@@ -0,0 +1,8 @@
+/// The interface and models for an API providing access to workout logs.
+library workout_log_api;
+
+export 'package:exercise_api/exercise_api.dart' show Exercise;
+
+export 'src/exceptions/exceptions.dart';
+export 'src/models/models.dart';
+export 'src/workout_log_api.dart';
