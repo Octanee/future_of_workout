8de5bceaa8868e890f750dfc28e986c4df31402b
Kamil Boryczka <leoctaneee@gmail.com>
2022-09-19 10:57:18 +0200

    create a custom exception for WorkoutLog API

---
+++ packages/workout_log_api/lib/src/exceptions/exceptions.dart
@@ -0,0 +1,4 @@
+import 'package:workout_log_api/workout_log_api.dart';
+
+/// Error thrown when a [WorkoutLog] with the given `id` is not found.
+class WorkoutLogNotFoundException implements Exception {}
