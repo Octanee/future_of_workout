bee9762ac0e02e8c0728cbc346cf67baccc95c10
Kamil Boryczka <leoctaneee@gmail.com>
2022-09-26 13:52:41 +0200

    Remove "Records" tab from "My Workouts"

--- lib/src/workouts/workouts_screen.dart
+++ lib/src/workouts/workouts_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:future_of_workout/src/exercise_records/workouts_records_tab.dart';
 import 'package:future_of_workout/src/widgets/widgets.dart';
 import 'package:future_of_workout/src/workout_list/workout_list.dart';
-import 'package:future_of_workout/src/workouts_logs/workouts_saved_tab.dart';
+import 'package:future_of_workout/src/workouts_logs/workouts_logs_tab.dart';
 
 class WorkoutsScreen extends StatelessWidget {
   const WorkoutsScreen({super.key});
@@ -26,21 +25,21 @@ class WorkoutsScreenView extends StatelessWidget {
 
   Widget _buildContent() {
     return DefaultTabController(
-      length: 3,
+      length: 2,
       child: Column(
         children: const [
           CustomTabBar(
             padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             tabs: [
               Tab(text: 'Created'),
-              Tab(text: 'Records'),
+              //Tab(text: 'Records'),
               Tab(text: 'Saved'),
             ],
           ),
           CustomTabBarView(
             children: [
               WorkoutsListTab(),
-              WorkoutsRecordsTab(),
+              //WorkoutsRecordsTab(),
               WorkoutsLogsTab(),
             ],
           )
