11638e766859629766284badf60dfb04b81ff2f9
Kamil Boryczka <leoctaneee@gmail.com>
2022-10-03 22:55:49 +0200

    Adding name to display

--- packages/workout_api/lib/src/models/series_intensity.dart
+++ packages/workout_api/lib/src/models/series_intensity.dart
@@ -7,36 +7,39 @@ enum SeriesIntensity {
   /// Very light
   ///
   /// `6` or more repetition in reserved
-  veryLight(value: 0.4),
+  veryLight(value: 0.4, name: 'Very light'),
 
   /// Light
   ///
   /// `4-5` repetition in reserved
-  light(value: 0.6),
+  light(value: 0.6, name: 'Light'),
 
   /// Moderate
   ///
   /// `2-3` repetition in reserved
-  moderate(value: 0.75),
+  moderate(value: 0.75, name: 'Moderate'),
 
   /// Vigorous
   ///
   /// `1` repetition in reserved
-  vigorous(value: 0.9),
+  vigorous(value: 0.9, name: 'Vigorous'),
 
   /// Maximum
   ///
   /// `0` repetition in reserved
-  maximum(value: 1),
+  maximum(value: 1, name: 'Maximum'),
 
   /// Too much
   ///
   /// Can not complete repetition
-  tooMuch(value: 1.1);
+  tooMuch(value: 1.1, name: 'Too much');
 
   ///{@macro series_intensity}
-  const SeriesIntensity({required this.value});
+  const SeriesIntensity({required this.value, required this.name});
 
   /// Value of series intensity.
   final double value;
+
+  /// Name to display.
+  final String name;
 }
