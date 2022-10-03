43605ccd47e7ef8390dcb1335ee9d30d181c6ff5
Kamil Boryczka <leoctaneee@gmail.com>
2022-10-03 16:59:59 +0200

    Genereate series log json serializable

--- packages/workout_log_api/lib/src/models/exercise_series_log.g.dart
+++ packages/workout_log_api/lib/src/models/exercise_series_log.g.dart
@@ -13,7 +13,7 @@ ExerciseSeriesLog _$ExerciseSeriesLogFromJson(Map<String, dynamic> json) =>
       weight: (json['weight'] as num).toDouble(),
       reps: json['reps'] as int,
       rest: json['rest'] as int,
-      intensity: $enumDecode(_$SeriesIntensityEnumMap, json['intensity']),
+      intensity: $enumDecode(_$SeriesLogIntensityEnumMap, json['intensity']),
       isFinished: json['isFinished'] as bool? ?? false,
     );
 
@@ -25,14 +25,14 @@ Map<String, dynamic> _$ExerciseSeriesLogToJson(ExerciseSeriesLog instance) =>
       'reps': instance.reps,
       'rest': instance.rest,
       'isFinished': instance.isFinished,
-      'intensity': _$SeriesIntensityEnumMap[instance.intensity]!,
+      'intensity': _$SeriesLogIntensityEnumMap[instance.intensity]!,
     };
 
-const _$SeriesIntensityEnumMap = {
-  SeriesIntensity.veryLight: 'veryLight',
-  SeriesIntensity.light: 'light',
-  SeriesIntensity.moderate: 'moderate',
-  SeriesIntensity.vigorous: 'vigorous',
-  SeriesIntensity.maximum: 'maximum',
-  SeriesIntensity.tooMuch: 'tooMuch',
+const _$SeriesLogIntensityEnumMap = {
+  SeriesLogIntensity.veryLight: 'veryLight',
+  SeriesLogIntensity.light: 'light',
+  SeriesLogIntensity.moderate: 'moderate',
+  SeriesLogIntensity.vigorous: 'vigorous',
+  SeriesLogIntensity.maximum: 'maximum',
+  SeriesLogIntensity.tooMuch: 'tooMuch',
 };
