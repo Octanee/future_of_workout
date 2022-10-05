d63c5eb895d707bf4367cb0872ef06494c9d0dc4
Kamil Boryczka <leoctaneee@gmail.com>
2022-10-05 15:53:51 +0200

    Adding colors for display body

--- lib/src/styles/app_colors.dart
+++ lib/src/styles/app_colors.dart
@@ -5,6 +5,20 @@ class AppColors {
 
   static const Color yellow = Color(0xFFFAA805);
 
+  static const Color body120 = Color(0xFFa81c1c);
+  static const Color body110 = Color(0xFFD22323);
+  static const Color body100 = Color(0xFFdb4f4f);
+  static const Color body90 = Color(0xFFFAA805);
+  static const Color body80 = Color(0xFFfbb11e);
+  static const Color body70 = Color(0xFFfbb937);
+  static const Color body60 = Color(0xFFfcc250);
+  static const Color body50 = Color(0xFFfccb69);
+  static const Color body40 = Color(0xFFfdd482);
+  static const Color body30 = Color(0xFFfddc9b);
+  static const Color body20 = Color(0xFFfee5b4);
+  static const Color body10 = Color(0xFFfeeecd);
+  static const Color body0 = Color(0xFFF2EFE6);
+
   static const Color background = Color(0xFFF2EFE6);
 
   static const Color white = Color(0xFFFAFAFA);
