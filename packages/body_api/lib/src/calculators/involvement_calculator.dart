21c5a3ec6aac532f2d46e5a05f387c8c310e39c1
Kamil Boryczka <leoctaneee@gmail.com>
2022-10-05 18:12:52 +0200

    Basic muscle involvement calculations

--- packages/body_api/lib/src/calculators/involvement_calculator.dart
+++ packages/body_api/lib/src/calculators/involvement_calculator.dart
@@ -1,10 +1,30 @@
-import 'dart:math';
+import 'dart:developer';
 
+import 'package:body_api/src/models/muscle.dart';
+
+/// Helper class to calculate a involvement for the muscle.
 class InvolvementCalculator {
   const InvolvementCalculator._();
 
   /// Calculate involvement
-  static int calculate() {
-    return Random().nextInt(120);
+  ///
+  /// MuscleInvolvement -> SeriesIntensity -> number of series
+  static int calculate({
+    required Muscle muscle,
+    required Map<double, Map<double, int>> data,
+  }) {
+    var involvement = 0.0;
+
+    data.forEach((muscleInvolvement, series) {
+      series.forEach((seriesIntensity, value) {
+        involvement += muscleInvolvement * seriesIntensity * value;
+      });
+    });
+
+    log('InvolvementCalculator -> involvement { $involvement }');
+
+    final value = involvement / (muscle.maximumRecoverableVolume / 2) * 100;
+
+    return value.toInt();
   }
 }
