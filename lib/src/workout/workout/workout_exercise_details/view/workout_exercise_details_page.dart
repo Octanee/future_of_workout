0c4b4a12bb13880b7850f4bc48a6dbf7ac17b014
Kamil Boryczka <leoctaneee@gmail.com>
2022-10-10 23:24:05 +0200

    Removing imports

--- lib/src/workout/workout/workout_exercise_details/view/workout_exercise_details_page.dart
+++ lib/src/workout/workout/workout_exercise_details/view/workout_exercise_details_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:future_of_workout/src/exercise/exercise.dart';
-import 'package:future_of_workout/src/styles/styles.dart';
 import 'package:future_of_workout/src/widgets/widgets.dart';
 import 'package:future_of_workout/src/workout/workout/workout.dart';
 import 'package:go_router/go_router.dart';
