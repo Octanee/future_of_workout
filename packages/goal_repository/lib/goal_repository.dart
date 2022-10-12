8c1a3ca9b4c6b2bbf4ecc7719870a0277e2c43ef
Kamil Boryczka <leoctaneee@gmail.com>
2022-10-12 16:46:19 +0200

    Goal repository

---
+++ packages/goal_repository/lib/goal_repository.dart
@@ -0,0 +1,6 @@
+/// A repository that handles goals related requests.
+library goal_repository;
+
+export 'package:goal_api/goal_api.dart' show Goal;
+
+export 'src/goal_repository.dart';
---
+++ packages/goal_repository/lib/src/goal_repository.dart
@@ -0,0 +1,32 @@
+import 'package:goal_api/goal_api.dart';
+
+/// {@template goal_repository}
+/// A repository that handles goal related requests.
+/// {@endtemplate}
+class GoalRepository {
+  /// {@macro goal_repository}
+  const GoalRepository({required GoalApi goalApi}) : _goalApi = goalApi;
+
+  final GoalApi _goalApi;
+
+  /// Provides a [Stream] of all goals
+  Stream<List<Goal>> getgoals() => _goalApi.getGoals();
+
+  /// Get the goal with the given `id`.
+  ///
+  /// If no goal with the given `id` exists,
+  /// a [GoalNotFoundException] error is thrown.
+  Goal get({required String id}) => _goalApi.get(id: id);
+
+  /// Saves a [Goal].
+  ///
+  /// If a [Goal] with the same `id` already exists,
+  /// it will be replaced.
+  Future<void> savegoal(Goal goal) => _goalApi.saveGoal(goal);
+
+  /// Deletes the `goal` with the given [id].
+  ///
+  /// If no goal with the given [id] exists,
+  /// a [GoalNotFoundException] erros is thrown.
+  Future<void> deleteGoal(String id) => _goalApi.deleteGoal(id);
+}
