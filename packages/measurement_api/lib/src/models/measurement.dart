ff111b3a1e0a3bb8c5c288a7b0d53f515faba40f
Kamil Boryczka <leoctaneee@gmail.com>
2022-10-17 15:25:36 +0200

    Change function name

--- packages/measurement_api/lib/src/models/measurement.dart
+++ packages/measurement_api/lib/src/models/measurement.dart
@@ -38,7 +38,7 @@ class Measurement extends Equatable {
   final Map<MeasurementPlace, double?> _bodyCircuits;
 
   /// Return value of measurement in specific place.
-  double? measurement({required MeasurementPlace place}) =>
+  double? circuit({required MeasurementPlace place}) =>
       _bodyCircuits[place];
 
   /// Copy instance with given values.
@@ -59,14 +59,14 @@ class Measurement extends Equatable {
     return Measurement(
       date: date ?? this.date,
       weight: weight ?? this.weight,
-      neckCircuit: neckCircuit ?? measurement(place: MeasurementPlace.neck),
-      chestCircuit: chestCircuit ?? measurement(place: MeasurementPlace.chest),
+      neckCircuit: neckCircuit ?? circuit(place: MeasurementPlace.neck),
+      chestCircuit: chestCircuit ?? circuit(place: MeasurementPlace.chest),
       bicepsCircuit:
-          bicepsCircuit ?? measurement(place: MeasurementPlace.biceps),
-      waistCircuit: waistCircuit ?? measurement(place: MeasurementPlace.waist),
-      bellyCircuit: bellyCircuit ?? measurement(place: MeasurementPlace.belly),
-      hipCircuit: hipCircuit ?? measurement(place: MeasurementPlace.hip),
-      calfCircuit: calfCircuit ?? measurement(place: MeasurementPlace.calf),
+          bicepsCircuit ?? circuit(place: MeasurementPlace.biceps),
+      waistCircuit: waistCircuit ?? circuit(place: MeasurementPlace.waist),
+      bellyCircuit: bellyCircuit ?? circuit(place: MeasurementPlace.belly),
+      hipCircuit: hipCircuit ?? circuit(place: MeasurementPlace.hip),
+      calfCircuit: calfCircuit ?? circuit(place: MeasurementPlace.calf),
     );
   }
 
