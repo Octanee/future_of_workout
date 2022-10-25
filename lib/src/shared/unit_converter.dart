7ba8b186860a21cfabcbb57e727166d74ab0085b
Kamil Boryczka <leoctaneee@gmail.com>
2022-10-25 13:40:41 +0200

    Change length convertion value

--- lib/src/shared/unit_converter.dart
+++ lib/src/shared/unit_converter.dart
@@ -2,7 +2,7 @@ class UnitConverter {
   const UnitConverter._();
 
   static const double _kilogramsToPoundsConversionValue = 2.204622622;
-  static const double _inchesToCentimetersConversionValue = 2.54;
+  static const double _centimetersToInchesConversionValue = 0.393700787402;
 
   static double kilogramsToPounds({required double weight}) {
     return weight * _kilogramsToPoundsConversionValue;
@@ -13,10 +13,10 @@ class UnitConverter {
   }
 
   static double centimetersToInches({required double length}) {
-    return length * (1 / _inchesToCentimetersConversionValue);
+    return length * _centimetersToInchesConversionValue;
   }
 
   static double inchesToCentimeters({required double length}) {
-    return length * _inchesToCentimetersConversionValue;
+    return length * (1 / _centimetersToInchesConversionValue);
   }
 }
