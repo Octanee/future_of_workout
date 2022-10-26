cef6c69b7788c811f68bea4cf048b6c82806b5d2
Kamil Boryczka <leoctaneee@gmail.com>
2022-10-26 20:44:37 +0200

    Change textStyle

--- lib/src/widgets/bold_text.dart
+++ lib/src/widgets/bold_text.dart
@@ -2,24 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:future_of_workout/src/styles/styles.dart';
 
 class BoldText extends StatelessWidget {
-  const BoldText({
+  BoldText({
     required this.boldText,
     this.mediumText = '',
     this.textAlign = TextAlign.start,
+    TextStyle? boldTextStyle,
+    TextStyle? mediumTextStyle,
     super.key,
-  });
+  })  : boldTextStyle = boldTextStyle ?? AppTextStyle.semiBold20,
+        mediumTextStyle = mediumTextStyle ?? AppTextStyle.medium16;
 
   final String boldText;
   final String mediumText;
   final TextAlign textAlign;
+  final TextStyle boldTextStyle;
+  final TextStyle mediumTextStyle;
+
   @override
   Widget build(BuildContext context) {
     return RichText(
       textAlign: textAlign,
       text: TextSpan(
         children: [
-          TextSpan(text: boldText, style: AppTextStyle.semiBold20),
-          TextSpan(text: mediumText, style: AppTextStyle.medium16),
+          TextSpan(text: boldText, style: boldTextStyle),
+          TextSpan(text: mediumText, style: mediumTextStyle),
         ],
       ),
     );
