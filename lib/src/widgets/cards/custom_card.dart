c733c91aa68e10514f090b823eb0a985604685b6
Kamil Boryczka <leoctaneee@gmail.com>
2022-11-07 18:22:41 +0100

    Better custom card

--- lib/src/widgets/cards/custom_card.dart
+++ lib/src/widgets/cards/custom_card.dart
@@ -1,32 +1,31 @@
-import 'package:flutter/material.dart';
-
-import 'package:future_of_workout/src/styles/styles.dart';
+import 'package:future_of_workout/src/common.dart';
 
 class CustomCard extends StatelessWidget {
   const CustomCard({
     required this.child,
-    this.padding,
+    this.padding = EdgeInsets.zero,
     this.onTap,
     super.key,
   });
 
   final Widget child;
-  final EdgeInsets? padding;
+  final EdgeInsets padding;
   final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return GestureDetector(
       onTap: onTap,
-      borderRadius: BorderRadius.circular(16),
-      child: Ink(
-        padding: padding,
+      child: DecoratedBox(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
           color: AppColors.white,
           boxShadow: [AppShadows.shadow15],
         ),
-        child: child,
+        child: Padding(
+          padding: padding,
+          child: child,
+        ),
       ),
     );
   }
